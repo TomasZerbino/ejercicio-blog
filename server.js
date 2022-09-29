1989817caa5360883f871ba0608c8f05560f0045
duarte896 <duarte.manuel10@gmail.com>
2022-09-29 15:37:21 -0300

    se agrega header + estilos y archivo de rutas

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const app = express();
+const routes = require("./routes");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-
-app.get("/", function(req, res){
-    res.send("holaaaaa");
-})
+app.use(routes);
 
 app.listen(3000, function () {
-    console.log("http://localhost:3000");
-})
+  console.log("http://localhost:3000");
+});
